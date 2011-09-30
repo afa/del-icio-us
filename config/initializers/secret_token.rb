288dd581e6a14125244dcbb57e394a312d832f28
afa <afa@afalone.local>
2011-09-30 13:56:47 +0400

    add rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DelIcioUs::Application.config.secret_token = 'de6f32cdfbc3de323f64e28f8eb3b3fbd945a75281c516f89a7e6771aa7d9fb3d5652c439413e22228d43761875eaabb84f1ec8f757459fa3d55a2dfaafd1ecc'
